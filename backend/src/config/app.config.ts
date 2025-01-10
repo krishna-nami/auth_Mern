53a7cb17070ae1becc505c1fd521c39413660f36
Krish <alpha@Krish-Nami.local>
2025-01-10 21:24:44 +1100

    second commint

---
+++ backend/src/config/app.config.ts
@@ -0,0 +1,14 @@
+import { getEnv } from "../common/utils/get-env";
+const appConfig = () => ({
+  NODE_ENV: getEnv("NODE_ENV", "developement"),
+  APP_ORIGIN: getEnv("APP_ORIGIN", "localhoast"),
+  PORT: getEnv("PORT", "5000"),
+  BASE_PATH: getEnv("BASE_PATH", "/api/v1"),
+  JWT: {
+    SECRET: getEnv("JWT_SECRET"),
+    EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "15m"),
+    REFRESH_SECRET: getEnv("JWT_REFRESH_SECRET"),
+    REFRESH_EXPIRES_IN: getEnv("JWT_REFRESH_IN", "30d"),
+  },
+});
+export const config = appConfig();
